8be6ea2c06cf734daed90ad7225eea4d11c3ec85
Nur Hidayatul Fatihah <nurhidayatulfatihah19@gmail.com>
2022-10-17 13:54:27 +0700

    membuat migrasi tabel

---
+++ app/Models/detailPemesanan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class detailPemesanan extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_10_17_082859_create_detail_pemesanans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pemesanan_id')->constrained('pemesanans');
+            $table->foreignId('buku_id')->constrained('bukus');
+            $table->integer('qty');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_pemesanans');
+    }
+};
