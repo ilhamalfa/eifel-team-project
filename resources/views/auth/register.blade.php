b07a7b20e2731a0cb0c64affb627afb14a8ca95a
ilhamalfa <kuro.nekogami22@gmail.com>
2022-10-19 14:08:06 +0700

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,6 +11,22 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+                        {{-- Username --}}
+                        <div class="row mb-3">
+                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- Nama --}}
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
