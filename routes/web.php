b7db0aa1a2df71393489ecddb98d7e8300bb2582
ilhamalfa <kuro.nekogami22@gmail.com>
2022-10-20 13:44:39 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
     return view('admin.dashboard');
 });
 
